ff582ab95c68e0fea3ab8519cdc373251f8809f4
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-07-18 18:48:35 +0700

    updated dependencies

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,3 +1,4 @@
+import '@reusable-ui/typos/dist/effects'
 import { styleSheet, cssVars, style, rule, atGlobal, vars, descendants, switchOf } from '@cssfn/core'
 import { basics, iconConfig } from '@reusable-ui/components'
 import { codes, colors, leads, typos, usesMildable, usesOutlineable } from '@reusable-ui/core'
