df796b57502632bf124d74cfe3c91887f995cee0
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-07-20 23:14:54 +0700

    patched StylesSSR

--- app/StylesSSR.tsx
+++ app/StylesSSR.tsx
@@ -1,11 +1,20 @@
-'use client'
-
-import { Styles } from '@cssfn/cssfn-react'
+import { useRef } from 'react'
 import { useServerInsertedHTML } from 'next/navigation'
+import { Styles } from '@cssfn/cssfn-react'
 
 
 
 export const StylesSSR = () => {
-    useServerInsertedHTML(() => <Styles key='123abc' />);
+    const isStyleInjected = useRef<boolean>(false); // workaround for <React.StrictMode>
+    useServerInsertedHTML(() => {
+        // conditions:
+        if (isStyleInjected.current) return null;
+        isStyleInjected.current = true;
+        
+        
+        
+        // actions:
+        return <Styles key='123abc' />
+    });
     return <></>;
 }
